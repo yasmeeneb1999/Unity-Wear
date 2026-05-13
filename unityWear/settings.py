3567a8ab5cbe77ac2cb5efd8ef0d3d1a83cd65a4
yasmeeneb1999 <yyasmina.klye@gmail.com>
2026-05-13 21:07:10 +0300

    finish project unity-wear

--- unityWear/settings.py
+++ unityWear/settings.py
@@ -74,17 +74,6 @@ WSGI_APPLICATION = "unityWear.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "library-db",  # Name of your PostgreSQL database
-        "USER": "postgres",
-        "PASSWORD": "123",
-        "HOST": "localhost",
-        "PORT": "5432",
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
@@ -144,9 +133,9 @@ else:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.getenv("DB_NAME"),  # same name as your .env file
+            "NAME": "library-db",  # Name of your PostgreSQL database
             "USER": "postgres",
-            "PASSWORD": os.getenv("DB_PASSWORD"),
+            "PASSWORD": "123",
             "HOST": "localhost",
             "PORT": "5432",
         }
